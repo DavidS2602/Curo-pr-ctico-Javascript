a72ff902b608679f9609e7d2940eae29723194ec
David Gaspar <davsangas@hotmail.com>
2022-06-15 12:37:20 -0500

    Página para descuentos

--- Descuentos.js
+++ Descuentos.js
@@ -1,22 +1,22 @@
-const precioOriginal = 120
-const descuento = 18
+//const precioOriginal = 120
+//const descuento = 18
 
-function calcularPrecioConDescuento(precio, descuento) {
-    const porcentajePrecioConDescuento = 100 - descuento
-    const precioConDescuento = (precio * porcentajePrecioConDescuento)/100
-    return precioConDescuento
+function calcularPrecioConDescuento(precio,descuento){
+    const porcentajePrecioConDescuento=100-descuento;
+    const precioConDescuento=(precio*porcentajePrecioConDescuento)/100;
+    return precioConDescuento;
 }
+function onClickButtonPriceDiscount()
+{
+    const inputPrice=document.getElementById("InputPrice");
+    const priceValue=inputPrice.value;
 
-function onclickButtonPriceDiscount() {
-    const InputPrice = document.getElementById("InputPrice")
-    const priceValue = InputPrice.value
+    const inputDiscount=document.getElementById("InputDiscount");
+    const discountValue=inputDiscount.value;
 
-    const InputDiscount = document.getElementById("InputDiscount")
-    const discountValue = InputDiscount.value
-    const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento)
-
-    const resultP = document.getElementById("ResultP")
-    resultP.innerText = "El precio con descuento es: $ " + precioConDescuento
+    const precioConDescuento=calcularPrecioConDescuento(priceValue,discountValue);
+    const resultP=document.getElementById("ResultP");
+    resultP.innerText="El precio con descuento son: $"+precioConDescuento;
 }
 
 
